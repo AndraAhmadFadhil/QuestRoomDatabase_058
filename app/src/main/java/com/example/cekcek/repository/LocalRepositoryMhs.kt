528a33dc00bfea941af4603ff87e761bf9cde4f9
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-17 00:19:53 +0700

    menambahkan object pada AlamatNavigasi untuk menandai view

--- app/src/main/java/com/example/cekcek/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/cekcek/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,24 @@ package com.example.activity10.repository
 
 import com.example.activity10.data.dao.MahasiswaDao
 import com.example.activity10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
     ) : RepositoryMhs {
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+
+        //getAllMhs
+        fun getAllMhs(): Flow<List<Mahasiswa>>
+
+        //getMhs
+        fun getMhs(nim: String): Flow<Mahasiswa>
+
+        //deleteMhs
+        suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+        //updateMhs
+        suspend fun updateMhs(mahasiswa: Mahasiswa)
     }
 }
