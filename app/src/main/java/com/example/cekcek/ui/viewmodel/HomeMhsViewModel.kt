e762d985dbec4bffaf31cf98c0c02d007d7c73b3
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-17 00:27:32 +0700

    menambahkan  ViewModel pada HomeMhsViewModel

--- app/src/main/java/com/example/cekcek/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/cekcek/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,2 +1,11 @@
 package com.example.cekcek.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.activity10.repository.RepositoryMhs
+import kotlinx.coroutines.flow.StateFlow
+
+class HomeMhsViewModel(
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+}
