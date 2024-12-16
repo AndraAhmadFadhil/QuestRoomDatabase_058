295eff06896471dda6b860ea045cfdd754f7cadc
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-17 01:54:16 +0700

    menambahkan data class DetailUiState pada file DetailMhsView

---
+++ app/src/main/java/com/example/cekcek/ui/view/mahasiswa/DetailMhsView.kt
@@ -0,0 +1,29 @@
+package com.example.cekcek.ui.view.mahasiswa
+
+import com.example.activity10.data.entity.Mahasiswa
+import com.example.activity10.ui.viewmodel.MahasiswaEvent
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+//memindahkan data dari entity ke ui
+fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent {
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
