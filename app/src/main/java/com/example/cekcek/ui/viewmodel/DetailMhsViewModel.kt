0968b6bf267b0397693f6fac32d5b6a81706a3af
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-17 00:56:12 +0700

    menambahkan Pengolahan State pada DetailMhsViewModel

--- app/src/main/java/com/example/cekcek/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/cekcek/ui/viewmodel/DetailMhsViewModel.kt
@@ -2,11 +2,19 @@ package com.example.cekcek.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.activity10.data.entity.Mahasiswa
 import com.example.activity10.repository.RepositoryMhs
 import com.example.activity10.ui.viewmodel.MahasiswaEvent
 import com.example.cekcek.ui.navigation.AlamatNavigasi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class DetailMhsViewModel(
     savedStateHandle: SavedStateHandle,
@@ -15,6 +23,34 @@ class DetailMhsViewModel(
     private val _nim: String = checkNotNull(savedStateHandle[AlamatNavigasi.DestinasiDetail.NIM])
 
     val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            )
+        )
+
 }
 
 
